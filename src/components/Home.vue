0575e248ced9e1809941f1a989ea09844a763013
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2017-10-31 18:03:17 +0100

    Started replacing web3 logic with truffle code

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,8 +17,8 @@
       <header>{{ contractProps.address }}</header>
       <section class="info">
         Beneficiary: {{ contractProps.beneficiary }} <br />
-        Goal: {{ contractProps.goal }} <br />
-        Value: {{ contractProps.value }} <br />
+        Goal: {{ contractProps.goal.toNumber() }} <br />
+        Value: {{ contractProps.value.toNumber() }} <br />
       </section>
 
       <section class="actions">
@@ -33,162 +33,10 @@
 
 <script>
 import Web3 from 'web3'
+import contract from 'truffle-contract'
+import BigNumber from 'bignumber.js'
 
-const contractAddress = '0x12340385532F2c1da854326B09DE458fC20FfF33'
-const abi = [
-  {
-    'constant': true,
-    'inputs': [],
-    'name': 'beneficiary',
-    'outputs': [{
-      'name': '',
-      'type': 'address',
-      'value': '0xfe49332023959dbd91b9ec78267dbecd6fae9410'
-    }],
-    'payable': false,
-    'stateMutability': 'view',
-    'type': 'function'
-  },
-  {
-    'constant': false,
-    'inputs': [],
-    'name': 'withdraw',
-    'outputs': [],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
-  },
-  {
-    'constant': true,
-    'inputs': [],
-    'name': 'value',
-    'outputs': [{
-      'name': '',
-      'type': 'uint256',
-      'value': '3000000000000000000'
-    }],
-    'payable': false,
-    'stateMutability': 'view',
-    'type': 'function'
-  },
-  {
-    'constant': true,
-    'inputs': [],
-    'name': 'goal',
-    'outputs': [{
-      'name': '',
-      'type': 'uint256',
-      'value': '10000000000000000000'
-    }],
-    'payable': false,
-    'stateMutability': 'view',
-    'type': 'function'
-  },
-  {
-    'constant': false,
-    'inputs': [],
-    'name': 'claim',
-    'outputs': [],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
-  },
-  {
-    'constant': true,
-    'inputs': [{
-      'name': '',
-      'type': 'address'
-    }],
-    'name': 'balanceOf',
-    'outputs': [{
-      'name': '',
-      'type': 'uint256',
-      'value': '0'
-    }],
-    'payable': false,
-    'stateMutability': 'view',
-    'type': 'function'
-  },
-  {
-    'constant': true,
-    'inputs': [],
-    'name': 'isClosed',
-    'outputs': [{
-      'name': '',
-      'type': 'bool',
-      'value': false
-    }],
-    'payable': false,
-    'stateMutability': 'view',
-    'type': 'function'
-  },
-  {
-    'inputs': [
-      {
-        'name': 'ifSuccessfulSendTo',
-        'type': 'address',
-        'index': 0,
-        'typeShort': 'address',
-        'bits': '',
-        'displayName': 'if Successful Send To',
-        'template': 'elements_input_address',
-        'value': '0xFe49332023959Dbd91B9ec78267dBECd6Fae9410'
-      },
-      {
-        'name': 'goalInEther',
-        'type': 'uint256',
-        'index': 1,
-        'typeShort': 'uint',
-        'bits': '256',
-        'displayName': 'goal In Ether',
-        'template': 'elements_input_uint',
-        'value': '10'
-      }
-    ],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'constructor'
-  },
-  {
-    'payable': true,
-    'stateMutability': 'payable',
-    'type': 'fallback'
-  },
-  {
-    'anonymous': false,
-    'inputs': [
-      {
-        'indexed': false,
-        'name': 'withdrawer',
-        'type': 'address'
-      },
-      {
-        'indexed': false,
-        'name': 'amount',
-        'type': 'uint256'
-      }
-    ],
-    'name': 'FundWithdrew',
-    'type': 'event'
-  },
-  {
-    'anonymous': false,
-    'inputs': [
-      {
-        'indexed': false,
-        'name': 'backer',
-        'type': 'address'
-      },
-      {
-        'indexed': false,
-        'name': 'amount',
-        'type': 'uint256'
-      }
-    ],
-    'name': 'FundAdded',
-    'type': 'event'
-  }
-]
+import BasicCrowdFundingArtifacts from '../../build/contracts/BasicCrowdFunding.json'
 
 export default {
   name: 'Home',
@@ -201,8 +49,8 @@ export default {
       wei: null,
       contractProps: {
         beneficiary: 0,
-        goal: 0,
-        value: 0,
+        goal: new BigNumber(0),
+        value: new BigNumber(0),
         address: 0
       }
       // contract, reactivity breaks assignment
@@ -216,6 +64,9 @@ export default {
   },
 
   created () {
+    let BasicCrowdFunding = contract(BasicCrowdFundingArtifacts)
+    BasicCrowdFunding.setProvider(Web3.givenProvider)
+
     if (Web3.givenProvider && !this.useLocalNode) {
       window.web3 = new Web3(Web3.givenProvider)
       console.log('Given provider', Web3.givenProvider)
@@ -224,47 +75,26 @@ export default {
       console.log('Connected to own node')
     }
 
-    window.web3.eth.getAccounts().then((accounts) => {
-      this.accounts = accounts
-      this.account = accounts[0]
+    BasicCrowdFunding.deployed().then((instance) => {
+      console.log(instance)
+      this.contract = instance
+      this.contractProps.address = this.contract.address
 
-      return window.web3.eth.getBalance(this.account)
-    }).then((result) => {
-      this.wei = result
-    }).catch((error) => {
-      console.log(error)
+      window.web3.eth.getAccounts().then((accounts) => {
+        this.accounts = accounts
+        this.account = accounts[0]
+
+        return window.web3.eth.getBalance(this.account)
+      }).then((result) => {
+        this.wei = result
+      }).catch((error) => {
+        console.log(error)
+      })
+
+      this.web3Contract = new window.web3.eth.Contract(BasicCrowdFundingArtifacts.abi, this.contract.address)
+
+      this._updateContract()
     })
-
-    this.contract = new window.web3.eth.Contract(abi, contractAddress)
-    this.contractProps.address = contractAddress
-
-    this.contract.events.FundAdded({}, (error, result) => {
-      if (error) {
-        console.error(error)
-        // We probably got here because the current provider doesn't support subscription
-        // So we might need some polling fallback
-      }
-    }).on('data', (log) => {
-      this.contractProps.value = parseInt(this.contractProps.value) + parseInt(log.returnValues.amount)
-      console.log(log)
-    }).on('error', (error) => {
-      console.log(error)
-    })
-
-    this.contract.events.FundWithdrew({}, (error, result) => {
-      if (error) {
-        console.error(error)
-        // We probably got here because the current provider doesn't support subscription
-        // So we might need some polling fallback
-      }
-    }).on('data', (log) => {
-      this.contractProps.value = parseInt(this.contractProps.value) - parseInt(log.returnValues.amount)
-      console.log(log)
-    }).on('error', (error) => {
-      console.log(error)
-    })
-
-    this._updateContract()
   },
 
   methods: {
@@ -272,7 +102,7 @@ export default {
     fundOneEther () {
       window.web3.eth.sendTransaction({
         from: this.account,
-        to: contractAddress,
+        to: this.contract.address,
         value: '10000000000000000'
       }).on('transactionHash', (hash) => {
         console.log(hash)
@@ -287,31 +117,35 @@ export default {
     },
 
     withdrawAll () {
-      this.contract.methods.withdraw().send({
+      this.contract.withdraw({
         from: this.account
-      }).on('error', (error) => {
-        console.log(error)
+      }).then((result) => {
+        console.log(result)
+      }).catch((e) => {
+        console.log(e)
       })
     },
 
     claim () {
-      this.contract.methods.claim().send({
+      this.contract.claim({
         from: this.account
-      }).on('error', (error) => {
-        console.log(error)
+      }).then((result) => {
+        console.log(result)
+      }).catch((e) => {
+        console.log(e)
       })
     },
 
     _updateContract () {
-      this.contract.methods.beneficiary().call().then((result) => {
+      this.contract.beneficiary.call().then((result) => {
         this.contractProps.beneficiary = result
       })
 
-      this.contract.methods.goal().call().then((result) => {
+      this.contract.goal.call().then((result) => {
         this.contractProps.goal = result
       })
 
-      this.contract.methods.value().call().then((result) => {
+      this.contract.value.call().then((result) => {
         this.contractProps.value = result
       })
     }
