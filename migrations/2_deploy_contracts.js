60320b66b6c8db83c4d6591c6ec5b1a18a0ed66e
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2017-11-17 17:43:07 +0100

    Removed ether divider and correctly calculate steps

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,12 @@
 const config = require('../src/config.js')
 
 const ether = 1000000000000000000
-const etherDivider = 100000
 
 const SolarParkFunding = artifacts.require('./SolarParkFunding.sol')
 const SolarToken = artifacts.require('./SolarToken.sol')
 
-const goal = config.crowdFunding.goal / etherDivider * ether
-const steps = config.crowdFunding.steps / etherDivider * ether
+const goal = config.crowdFunding.goal * ether
+const steps = config.crowdFunding.goal / config.crowdFunding.steps * ether
 
 const totalTokens = goal / steps
 
