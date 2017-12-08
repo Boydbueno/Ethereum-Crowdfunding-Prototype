9027125aadfdbc16e0ca79858ff59d6f9efb67bc
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2017-12-08 15:28:19 +0100

    Allow deploying to ropsten testnet

--- truffle.js
+++ truffle.js
@@ -1,9 +1,17 @@
+let HDWalletProvider = require('truffle-hdwallet-provider')
+let config = require('./config')
+
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
       network_id: '*', // Match any network id
+      gas: 4600000
+    },
+    ropsten: {
+      provider: new HDWalletProvider(config.dev.env.MNEMONIC, 'https://ropsten.infura.io/'),
+      network_id: 3,
       gas: 4600000
     }
   }
