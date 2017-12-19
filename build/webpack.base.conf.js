e7db7b3f87ffbb6a527c4dac50cd7806406c2d67
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2017-12-19 17:57:51 +0100

    Fixed font path in new build, hopefully

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -72,7 +72,9 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: '/' + utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: '../../../' + utils.assetsPath('fonts/') + '[name].[hash:7].[ext]',
+          outputPath: utils.assetsPath('fonts/'),
+          useRelativePath: true
         }
       }
     ]
